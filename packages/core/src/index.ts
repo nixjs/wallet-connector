51bed90fb33a4953b4f53d0b4177bef1db3df4a4
nghinv <nghinv@inpirelab.io>
2022-10-07 13:06:56 +0700

    fix: export tronlink provider

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,15 +1,16 @@
-export * from './provider/BaseProvider'
-export * from './provider/WalletProvider'
-export * from './provider/MetaMaskProvider'
-export * from './provider/BinanceProvider'
-export * from './provider/Coin98Provider'
-export * from './provider/WalletConnect'
+export * from "./provider/BaseProvider";
+export * from "./provider/WalletProvider";
+export * from "./provider/MetaMaskProvider";
+export * from "./provider/BinanceProvider";
+export * from "./provider/Coin98Provider";
+export * from "./provider/TronLinkProvider";
+export * from "./provider/WalletConnect";
 
-export * from './tools/helpers'
-export * from './tools/utils'
+export * from "./tools/helpers";
+export * from "./tools/utils";
 
-export * from './types'
-export * from './interfaces'
-export * from './constants'
+export * from "./types";
+export * from "./interfaces";
+export * from "./constants";
 
-export { ethers } from 'ethers'
+export { ethers } from "ethers";
